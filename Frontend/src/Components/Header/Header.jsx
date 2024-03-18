b12af691e273645a134e92feae88d3c448640ef1
olatz <gotiolatz@gmail.com>
2024-03-18 20:37:08 +0100

    Header-creation

---
+++ Frontend/src/Components/Header/Header.jsx
@@ -0,0 +1,32 @@
+import { useNavigate, Link } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import { useEffect, useState } from 'react'
+import {
+    HomeOutlined,
+    LogoutOutlined,
+    LoginOutlined,
+    IdcardOutlined,
+    AuditOutlined,
+} from '@ant-design/icons'
+import { Menu, Input } from 'antd'
+// PENDING import { logout } from '../../authSlice' ???
+
+
+const TheHeader = () => {
+
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+
+    
+
+
+
+
+
+
+
+
+
+}
+
+export default TheHeader

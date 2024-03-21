9d8e38e4153c3b4f82f22b199816a3b71dcdb921
AdrikLarreategui <adrian15186@hotmail.com>
2024-03-21 18:11:10 +0100

    commit cambios en app.jsx, y profile. Creación de Admin, NotFound, Profile, Users, User, AdminZone y PrivateZone

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from "react";
 import Register from "./assets/components/Register/Register";
-
 import TheHeader from "./assets/components/TheHeader/TheHeader";
 import Home from "../src/assets/components/Home/Home";
+import Profile from '../src/assets/components/Profile/Profile'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
 import Login from "./assets/components/Login/Login";
+import Admin from "./assets/components/Admin/Admin";
+import AdminZone from "./guards/AdminZone";
+import PrivateZone from "./guards/PrivateZone";
+import NotFound from "./assets/components/NotFound/NotFound";
+import "./App.css";
+
 
 function App() {
   return (
@@ -17,6 +22,9 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/profile" element= {<PrivateZone><Profile /></PrivateZone>}/>
+          <Route path="/admin" element={<AdminZone><Admin /></AdminZone>}/>
+          <Route path="*" element={<NotFound />}/>
         </Routes>
       </BrowserRouter>
     </div>

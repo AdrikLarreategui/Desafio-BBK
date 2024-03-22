b26050928658a76602d469f189597d47aa89295d
olatz <gotiolatz@gmail.com>
2024-03-22 20:04:28 +0100

    formTalent

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from "react";
 import Register from "./assets/components/Register/Register";
-
+import Login from "./assets/components/Login/Login";
 import TheHeader from "./assets/components/TheHeader/TheHeader";
 import Home from "../src/assets/components/Home/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import TalentRegister from './assets/components/TalentRegister/TalentRegister'
 import "./App.css";
-import TheHeader from "./assets/Components/Header/Header";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./assets/Components/Login/Login";
-import Home from './assets/Components/Home/Home'
+
 
 function App() {
   return (
@@ -19,6 +17,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/registertalent" element={<TalentRegister />} />
           <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>

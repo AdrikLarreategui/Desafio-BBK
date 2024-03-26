99ad5f100f47f4358c2a5e9770f0d05a779f9f93
AdrikLarreategui <adrian15186@hotmail.com>
2024-03-26 08:59:48 +0100

    commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,11 @@
 import { useState } from "react";
 import Register from "./assets/components/Register/Register";
-import Login from "./assets/components/Login/Login";
 import TheHeader from "./assets/components/TheHeader/TheHeader";
 import Home from "../src/assets/components/Home/Home";
 import Profile from '../src/assets/components/Profile/Profile'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
 import Login from "./assets/components/Login/Login";
+
 
 function App() {
   return (
@@ -17,7 +16,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/registertalent" element={<TalentRegister />} />
           <Route path="/login" element={<Login />} />
           <Route path="/profile" element= {<PrivateZone><Profile /></PrivateZone>}/>
           <Route path="/admin" element={<AdminZone><Admin /></AdminZone>}/>

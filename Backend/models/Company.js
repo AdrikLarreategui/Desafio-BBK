4ab382902c5362263f6e553e2b71788a742f6e58
olatz <gotiolatz@gmail.com>
2024-04-12 12:32:50 +0200

    backCompany

--- Backend/models/Company.js
+++ Backend/models/Company.js
@@ -15,54 +15,54 @@ const CompanySchema = new mongoose.Schema({
     type: String,
   },
   tokens: [],
-  companyProfile: {
-    companyName: String,
-    CIF: {
-      type: String,
-      // unique: true,
-      //required:[true, 'CIF  necesario para registrarse']
-      //LO PONGO ASÍ PARA PRUEBAS, LUEGO HABRÁ QUE HACER QUE SEA DE UNA LONGITUD CONCRETA: 9 CARACTERES
-    },
-    typeCompany: {
-      type: String,
-      enum: [
-        "Empresa individual",
-        "Sociedad limitada (S.L.)",
-        "Sociedad Anónima (S.A.)",
-        "Asociación sin ánimo de lucro",
-        "Sociedad Colectiva",
-        "Sociedad Comanditaria",
-        "Comunidad de Bienes",
-        "Sociedad Cooperativa",
-      ],
-      //  required: [true, "Es obligatorio incluir el tipo de empresa"]
-    },
-    field: {
-      type: String,
-      enum: [
-        "Sector primario",
-        "Sector secundario",
-        "Sector terciario",
-        "Sector cuaternario",
-        "Sector quinario",
-      ],
-      //   required: [true, "Es obligatorio incluir el sector al que pertenece"],
-    },
-    workersNumber: String,
-    description: String,
-    location: String,
-    telephoneNumber: String,
-    website: String,
-    //Luego hacer que incluya .com/.es/.eus
-    createdOffers: [
-      /*{ type: ObjectId, ref: "Offer" }*/
-    ],
-    likedTalents: [
-      {
-        //    type: ObjectId, ref:"Talent"
-      },
-    ],
+
+  CIF: {
+    type: String,
+    // unique: true,
+    //required:[true, 'CIF  necesario para registrarse']
+    //LO PONGO ASÍ PARA PRUEBAS, LUEGO HABRÁ QUE HACER QUE SEA DE UNA LONGITUD CONCRETA: 9 CARACTERES
   },
+  companyName: String,
+  typeCompany: {
+    type: String,
+    enum: [
+      "Empresa individual",
+      "Sociedad limitada (S.L.)",
+      "Sociedad Anónima (S.A.)",
+      "Asociación sin ánimo de lucro",
+      "Sociedad Colectiva",
+      "Sociedad Comanditaria",
+      "Comunidad de Bienes",
+      "Sociedad Cooperativa",
+    ],
+    //  required: [true, "Es obligatorio incluir el tipo de empresa"]
+  },
+  field: {
+    type: String,
+    enum: [
+      "Sector primario",
+      "Sector secundario",
+      "Sector terciario",
+      "Sector cuaternario",
+      "Sector quinario",
+    ],
+    //   required: [true, "Es obligatorio incluir el sector al que pertenece"],
+  },
+  workersNumber: String,
+  description: String,
+  location: String,
+  telephoneNumber: String,
+  website: String,
+  //Luego hacer que incluya .com/.es/.eus
+  createdOffers: [
+    /*{ type: ObjectId, ref: "Offer" }*/
+  ],
+  likedTalents: [
+    {
+      //    type: ObjectId, ref:"Talent"
+    },
+  ],
+
 });
 
 CompanySchema.methods.toJSON = function () {

4b6cb26ca5c23db4dafa35458d0d24c33708c19b
AdrikLarreategui <adrian15186@hotmail.com>
2024-04-14 12:36:15 +0200

    commit cambio de puerto al 8080

--- Frontend/src/assets/redux/auth/talentAuthService.js
+++ Frontend/src/assets/redux/auth/talentAuthService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000";
+const API_URL = "http://localhost:8080";
 
 const registerTalent = async (userData) => {
   const res = await axios.post(`${API_URL}/talents/`, userData);

6ab57d69e800d18f29a1cd468c320591c39eafc0
olatz <gotiolatz@gmail.com>
2024-04-16 22:01:15 +0200

    designHomeAndLoginsWithPhotos

--- Frontend/src/assets/components/Login/Login.jsx
+++ Frontend/src/assets/components/Login/Login.jsx
@@ -1,12 +1,17 @@
 import React, { useState } from "react";
 //import { Button, Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
+const foto9 = "../images/foto9.jpg"
 import "./Login.css"
 
 const Login = () => {
   return (
     <>
       <h2>Login</h2>
+    <section className="firstLogin">
+      <article className="foto9">
+        <img src={foto9} alt="" />
+      </article>
 
       <div>
         <Link to="/talent/login">
@@ -15,8 +20,8 @@ const Login = () => {
         <Link to="/company/login">
           <button className="company">ORGANIZACIÓN</button>
         </Link>
-     
       </div>
+    </section>
     </>
   );
 };
